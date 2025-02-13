30573ab439fec32f239e9af7196b4c3b9151edde
Sveticusya <slpavlenko3005@gmail.com>
2025-02-13 12:15:30 +0400

    HomeWork11

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,48 @@
-import java.io.IOException;
-import java.util.Random;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File folder = new File(".");
+        File[] files = folder.listFiles();
+        if(files != null) {
+            HashMap<String, File> textFiles = new HashMap<String, File>();
+            for (File file : files) {
+                String fileName = file.getName();
+                if (fileName.contains(".txt")) {
+                    textFiles.put(fileName, file);
+                    System.out.println(file.getName());
+                }
+            }
+
+            Scanner s = new Scanner(System.in);
+            File selectedFile = null;
+            while (selectedFile == null) {
+                System.out.println("Выберите файл.");
+                String selectedFileName = s.nextLine();
+                if(textFiles.containsKey(selectedFileName)) {
+                    selectedFile = textFiles.get(selectedFileName);
+                }
+                else
+                    System.out.println("Файла с таким именем не существует");
+            }
+
+            FileInputStream fis= new FileInputStream(selectedFile);     //opens a connection to an actual file
+            System.out.println("Содержимое файла: ");
+            int r=0;
+            while((r=fis.read())!=-1) {
+                System.out.print((char) r);      //prints the content of the file
+            }
+            System.out.println("");
+
+            System.out.println("Введите текст: ");
+            String textToAdd = s.nextLine();
+            FileOutputStream fos = new FileOutputStream(selectedFile, true);
+            fos.write(textToAdd.getBytes(), 0, textToAdd.length());
+        }
     }
 }
